14b415202f2692e1296c9676f01e7ad21512e9b3
Katsuya Endoh <studio@enkatsu.org>
2023-04-01 19:14:48 +0900

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const balls = [];
 
 function setup() {
     const canvasParent = document.getElementById('canvas-parent');
@@ -5,9 +6,34 @@ function setup() {
     const sketchCanvas = createCanvas(canvasWidth, 450);
     sketchCanvas.parent('canvas-parent');
 
-    background(100);
+    for (let i =0; i < 50; i++) {
+        const r = random(10, 30);
+        balls.push({
+            x: random(r, width - r), y: random(r, height - r),
+            sx: random(-2.0, 2.0), sy: random(-2.0, 2.0),
+            r: r,
+            c: color(100, 200, 250, 150),
+        });
+    }
 }
 
 function draw() {
-    ellipse(mouseX, mouseY, 10, 10);
+    background(200);
+
+    for (let ball of balls) {
+        ball.x += ball.sx;
+        ball.y += ball.sy;
+        if (ball.r > ball.x || ball.x > width - ball.r) {
+            ball.sx *= -1.0;
+            ball.x = constrain(ball.x, ball.r, width - ball.r);
+        }
+        if (ball.r > ball.y || ball.y > height - ball.r) {
+            ball.sy *= -1.0;
+            ball.y = constrain(ball.y, ball.r, height - ball.r);
+        }
+
+        noStroke();
+        fill(ball.c);
+        ellipse(ball.x, ball.y, ball.r * 2, ball.r * 2);
+    }
 }
